adaff3bb2a1761016a0864a01362bc78400e4353
saranya <saranyasubramaniam@gmail.com>
2020-06-25 08:18:12 -0700

    first commit in git repo

---
+++ FirstTestNGPro/NewTestNGClass.java
@@ -0,0 +1,19 @@
+package FirstTestNGPro;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTestNGClass {
+  @Test
+  public void googleTest() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
